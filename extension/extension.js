25beb9a7f5aa0519f3bf2b09682b52f666555c8b
eraldcafka <erald.c@scopicsoftware.com>
2018-06-04 14:01:19 +0200

    + Added responsible script which will manage extension screen capture functionality

---
+++ extension/extension.js
@@ -0,0 +1,30 @@
+"use strict";
+
+chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
+	switch (message & message.type) {
+		case 'getUserScreen':
+			handleGetUserScreenRequest(message.sources, sender.tab, sendResponse);
+			break;
+		default:
+			sendResponse({
+				type: 'error',
+				message: 'Unrecognized request',
+			});
+	}
+});
+
+function handleGetUserScreenRequest(sources, tab, sendResponse) {
+	chrome.desktopCapture.chooseDesktopMedia(sources, tab, streamId => {
+		if (!streamId) {
+			sendResponse({
+				type: 'error',
+				message: 'Failed to get stream ID',
+			});
+		} else {
+			sendResponse({
+				type: 'success',
+				streamId: streamId,
+			});
+		}
+	});
+}
